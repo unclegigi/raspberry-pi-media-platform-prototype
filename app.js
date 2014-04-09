895f28ac9ef5e8487b0c3cffe5e19f3128626239
Uncle Gigi <unclegigi@outlook.com>
2014-04-09 11:15:15 +0200

    add test links to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ omx = require('omxcontrol');
 app.use(omx());
 
 app.get('/', function(req, res){
-    res.send("OMX")
+    res.send("<a href='http://localhost/omx/start/Test.mp4'>Play Test.mp4</a><br /><a href='http://localhost/omx/pause'>Pause</a><br /><a href='http://localhost/omx/quit'>Quit</a>")
 });
 
 app.listen(80);
