131d45125e8862922bc0d8a397f126b4486d6091
Uncle Gigi <unclegigi@outlook.com>
2014-04-10 18:32:15 +0200

    add get routes to app.js

--- source/app.js
+++ source/app.js
@@ -5,7 +5,22 @@ omx = require('omxcontrol');
 app.use(omx());
 
 app.get('/', function(req, res){
-    res.sendfile("./views/index.html")
+    res.sendfile("./views/index.html");
+});
+
+app.get("/player/start/:file", function(req, res) {
+	console.log(req.params.file);
+	res.end();
+});
+
+app.get("/player/stop/:file", function(req, res) {
+	console.log(req.params.file);
+	res.end();
+});
+
+app.get("/player/pause/:file", function(req, res) {
+	console.log(req.params.file);
+	res.end();
 });
 
 app.listen(3000);
